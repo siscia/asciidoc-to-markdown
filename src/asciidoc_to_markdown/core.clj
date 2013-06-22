a23d89896f8b757ed8966342e39393753163f0c4
Simone Mosciatti <simone@mweb.biz>
2013-06-22 18:03:12 +0200

    manage jekyll

--- src/asciidoc_to_markdown/core.clj
+++ src/asciidoc_to_markdown/core.clj
@@ -1,4 +1,5 @@
-(ns asciidoc-to-markdown.core)
+(ns asciidoc-to-markdown.core
+  (:use [clojure.tools.cli :only [cli]]))
 
 (def titles-regex
   (sorted-map-by #(> (count %1) (count %2))
@@ -15,19 +16,27 @@
           text
           (keys titles-regex)))
 
-(defn source [text]
+(defn source [text & jekyll]
   (clojure.string/replace text
                           #"\[source,(.*?)\]\n----\n(.*?\n+.*?)\n----"
-                          "```$1\n$2```\n"))
+                          (if (first jekyll)
+                            "{% highlight $1 %}\n$2\n{% endhighlight %}"
+                            "``` $1\n$2\n```")))
 
 (defn inline-code [text]
   (clojure.string/replace text
                           #"\+(.*?)\+"
                           "`$1`"))
 
-(defn -main [& [input output]]
-  (spit output (-> input
-                   slurp
-                   source
-                   title
-                   inline-code)))
+(defn -main [input output & args]
+  (let [[args opts banner]
+        (cli args
+             ["-h" "--help" "Show help" :default false]
+             ["-j" "--jekyll" "Make jekyll ready markdown file" :flag true :default true])]
+    (println args opts banner)
+    (println (:jekyll args))
+    (spit output (-> input
+                     slurp
+                     (source (:jekyll args))
+                     title
+                     inline-code))))
