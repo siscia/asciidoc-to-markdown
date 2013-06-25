3c8662c586ebcf5d59bdc5391ac1f878e6e1189b
Simone Mosciatti <simone@mweb.biz>
2013-06-25 15:54:41 +0200

    make it works with code in multiple linesss

--- src/asciidoc_to_markdown/core.clj
+++ src/asciidoc_to_markdown/core.clj
@@ -18,25 +18,37 @@
 
 (defn source [text & jekyll]
   (clojure.string/replace text
-                          #"\[source,(.*?)\]\n----\n(.*?\n+.*?)\n----"
+                          ;#"\[source,(.*?)\]\n----\n(.*?\n*+.*)\n----"
+                          ;#"\[source,(.*?)\]\n----\n([\s\S])\n----"
+                          ;#"\[source,(.*)\][\s\S]----[\s\S](?s:(.*))----"
+                          ;#"\[source,(.*)\][\s\S]----[\s\S]([^-{4}]+)"
+                          ;#"\[source,(.*)\][\s\S]----[\s\S]^(?:(?!----).)+$"
+                          #"\[source,(.*)\][\s\S]----[\s\S](?s:(.*?))----"
                           (if (first jekyll)
-                            "{% highlight $1 %}\n$2\n{% endhighlight %}"
-                            "``` $1\n$2\n```")))
+                            "{% highlight $1 %}\n$2{% endhighlight %}"
+                            "``` $1\n$2```")))
 
 (defn inline-code [text]
   (clojure.string/replace text
                           #"\+(.*?)\+"
                           "`$1`"))
 
+(defn meta-info [text]
+  (clojure.string/replace text
+                          #"^.*(Author:|Email:|Date:|Revision:).*$"
+                          ""))
+
 (defn -main [input output & args]
   (let [[args opts banner]
         (cli args
              ["-h" "--help" "Show help" :default false]
              ["-j" "--jekyll" "Make jekyll ready markdown file" :flag true :default true])]
-    (println args opts banner)
-    (println (:jekyll args))
+    ;(println args opts banner)
+    ;(println (:jekyll args))
     (spit output (-> input
                      slurp
                      (source (:jekyll args))
                      title
-                     inline-code))))
+                     inline-code
+                     ;meta-info
+                     ))))
